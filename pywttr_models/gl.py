1cab0152e8e25937f52fdabf7c20afe13feb0803
monosans <hsyqixco@protonmail.com>
2022-11-26 22:33:48 +0300

    Add support for Galician language

---
+++ pywttr_models/gl.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import List
+
+from . import base
+
+
+class CurrentConditionItem(base.CurrentConditionItem):
+    lang_gl: List[base.LangItem]
+
+
+class HourlyItem(base.HourlyItem):
+    lang_gl: List[base.LangItem]
+
+
+class WeatherItem(base.WeatherItem):
+    hourly: List[HourlyItem]
+
+
+class Model(base.Model):
+    current_condition: List[CurrentConditionItem]
+    weather: List[WeatherItem]
